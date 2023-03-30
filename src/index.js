2130890490c72b5c42dcd23e6ec7ba322c895a2e
Lenna <pampam 32121@gmail.com>
2023-03-30 18:48:11 +0300

    added random number btn and used action.payload

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { createStore } from 'redux';
 
 // const initialState = 0;
 
-const reducer = (state = 0, action) => {
+const reducer = (state = 1, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
     case 'DEC': 
       return state - 1;
+    case 'RND': 
+      return state * action.payload;
     default:
       return state;
   }
@@ -29,6 +31,12 @@ document.getElementById('inc').addEventListener('click', () => {
 document.getElementById('dec').addEventListener('click', () => {
   store.dispatch({type: 'DEC'});
 })
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 11);
+  store.dispatch({type: 'RND', payload: value});
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
