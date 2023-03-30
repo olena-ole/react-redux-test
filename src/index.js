005c6d916922c3b854ffce682d6e2b7ee31e07de
Lenna <pampam 32121@gmail.com>
2023-03-30 17:34:34 +0300

    imitated redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-console.log('Hello redux!');
+const initialState = 0;
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC': 
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+let state = reducer(initialState, {type: 'INC'});
+state = reducer(state, {type: 'INC'});
+state = reducer(state, {type: 'INC'});
+state = reducer(state, {type: 'INC'});
+
+console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
