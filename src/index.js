bffae1132c0c6e7a5f5a919e31b761656e6a854d
Lenna <pampam32121@gmail.com>
2023-03-31 12:17:55 +0300

    connected react insted of using DOM api

--- src/index.js
+++ src/index.js
@@ -3,38 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './Counter';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = createStore(reducer);
 const { dispatch, subscribe, getState } = store;
 
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 const update = () => {
-  document.getElementById('counter').textContent = getState().value;
+  root.render(
+    <Counter 
+      counter={getState().value} 
+      inc={inc} 
+      dec={dec} 
+      rnd={() => {
+        const value = Math.floor(Math.random() * 10);
+        rnd(value);
+      }}
+    />
+  );
 }
 
+update();
 subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-
-// const incDispatch = bindActionCreators(inc, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-  
-  </>
-);
 
