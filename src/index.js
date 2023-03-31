e10e19d4eb43b7c9a894c01b23c396a14eeb9e1a
Lenna <pampam32121@gmail.com>
2023-03-31 09:22:07 +0300

    used bindActionCreator to optimize index file

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ const update = () => {
 
 subscribe(update);
 
-const incDispatch = () => dispatch(inc());
-const decDispatch = () => dispatch(dec());
-const rndDispatch = (value) => dispatch(rnd(value));
+const bindActionCreator = (creator, dispatch) => (...args) => {
+  dispatch(creator(...args));
+}
+
+const incDispatch = bindActionCreator(inc, dispatch);
+const decDispatch = bindActionCreator(dec, dispatch);
+const rndDispatch = bindActionCreator(rnd, dispatch);
 
 document.getElementById('inc').addEventListener('click', incDispatch);
 document.getElementById('dec').addEventListener('click', decDispatch);
