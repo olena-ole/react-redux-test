b413e6f9e2967d40147f2f7a9ba44b08764ecc7c
Lenna <pampam32121@gmail.com>
2023-03-31 10:40:11 +0300

    optimized actions and using bindActionCreators

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions';
+import * as actions from './actions';
 
 const store = createStore(reducer);
 const { dispatch, subscribe, getState } = store;
@@ -17,21 +17,17 @@ subscribe(update);
 //   dispatch(creator(...args));
 // }
 
-const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
-  incDispatch: inc,
-  decDispatch: dec,
-  rndDispatch: rnd
-}, dispatch);
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
 // const incDispatch = bindActionCreators(inc, dispatch);
 // const decDispatch = bindActionCreators(dec, dispatch);
 // const rndDispatch = bindActionCreators(rnd, dispatch);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
-document.getElementById('dec').addEventListener('click', decDispatch);
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  rndDispatch(value);
+  rnd(value);
 });
 
 
