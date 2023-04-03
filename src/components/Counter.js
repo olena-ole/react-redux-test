62419d24cec743bb8b2e5a8afa35908343bd4368
Lenna <pampam32121@gmail.com>
2023-04-03 15:26:26 +0300

    added mapDispatchToProps to add functions as props to Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,6 @@
 import { connect } from "react-redux";
+import { inc, dec, rnd } from '../actions';
+
 const Counter = ({counter, inc, dec, rnd}) => {
     return (
         <div className="jumbotron">
@@ -16,7 +18,18 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = (dispatch) => {
+    return { 
+        inc: () => dispatch(inc()), 
+        dec: () => dispatch(dec()), 
+        rnd: () => {
+            const value = Math.floor(Math.random() * 10 + 1);
+            dispatch(rnd(value));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 // const update = () => {
 //   root.render(
